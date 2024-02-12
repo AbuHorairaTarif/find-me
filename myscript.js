22dd25b185efe1864aa8b34c5696ae7064f7072b
AbuHorairaTarif <horaira.cse12@gmail.com>
2024-02-12 16:11:59 +0600

    zoom in/out feature added

--- myscript.js
+++ myscript.js
@@ -1,30 +1,33 @@
 // script.js
 
-var map = L.map('map').setView([0, 0], 19);
+var map = L.map('map').setView([0, 0], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
 var marker = L.marker([0, 0]).addTo(map);
-var polyline = L.polyline([]).addTo(map); // Initialize an empty polyline
+var polyline = L.polyline([]).addTo(map);
 
 function updateLocation(position) {
     var latlng = [position.coords.latitude, position.coords.longitude];
-
-    // Update marker position
     marker.setLatLng(latlng);
 
-    // Update and redraw the polyline with the new position
     var latlngs = polyline.getLatLngs();
     latlngs.push(latlng);
     polyline.setLatLngs(latlngs);
-
-    // Pan the map to the updated position
     map.panTo(latlng);
 }
 
 function handleLocationError(error) {
     console.error(`Error getting location: ${error.message}`);
+}
+
+function zoomIn() {
+    map.zoomIn();
+}
+
+function zoomOut() {
+    map.zoomOut();
 }
 
 if (navigator.geolocation) {
