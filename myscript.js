d62774800deceeec9a6a4f978cc012c554d60dd8
AbuHorairaTarif <horaira.cse12@gmail.com>
2024-02-13 10:45:46 +0600

    updated current marker and line group

--- myscript.js
+++ myscript.js
@@ -5,16 +5,27 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-var marker = L.marker([0, 0]).addTo(map);
-var polyline = L.polyline([]).addTo(map);
+var startMarker = L.marker([0, 0]).addTo(map);
+var currentMarker = L.marker([0, 0]).addTo(map);
+
+var lineGroup = L.layerGroup().addTo(map);
 
 function updateLocation(position) {
     var latlng = [position.coords.latitude, position.coords.longitude];
-    marker.setLatLng(latlng);
 
-    var latlngs = polyline.getLatLngs();
-    latlngs.push(latlng);
-    polyline.setLatLngs(latlngs);
+    // Update current marker position
+    currentMarker.setLatLng(latlng);
+
+    // Create and append a line to connect the start and current positions
+    if (startMarker.getLatLng().equals([0, 0])) {
+        startMarker.setLatLng(latlng);
+    } else {
+        var line = L.polyline([startMarker.getLatLng(), latlng], { color: 'blue' });
+        lineGroup.addLayer(line);
+        startMarker.setLatLng(latlng);
+    }
+
+    // Pan the map to the updated position
     map.panTo(latlng);
 }
 
